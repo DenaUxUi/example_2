258d9aed2e21315dc9d7274e8d82afdf73f1ed95
root <root@DELLG155511.>
2025-04-16 20:11:36 +0600

    Second answer

---
+++ server.ts
@@ -0,0 +1,13 @@
+// server.ts
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
